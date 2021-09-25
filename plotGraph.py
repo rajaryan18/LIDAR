027a70fcc36b74e477069f41e09a0ec118aac0e3
Raj Aryan <73771884+rajaryan18@users.noreply.github.com>
2021-09-25 21:39:26 +0530

    Plotting works

--- plotGraph.py
+++ plotGraph.py
@@ -4,24 +4,21 @@ import math
 
 plt.ion()
 fig = plt.figure()
-plt.axis([-3, 3, -3, 3])
-ser = serial.Serial('/dev/ttyACMo', 9600)
-ser.close()
-ser.open()
+plt.axis([-9, 9, -9, 9])
+plt.scatter(0, 0, c="black", marker='s', s=5.5)
+ser = serial.Serial('COM4', 9600)
 angle = 0
-
 while True:
-	data = ser.readline()
-	print(data.decode())
-	r = float(data.decode())
-	#r = float(random.randint(0, 20))/10
-	angle = angle + 1.8
-	if angle >= 360.0:
-		angle = angle - 360
-		plt.clf()
-	sine = math.sin(angle)
-	cosine = math.cos(angle) 
-	plt.scatter(r*cosine, r*sine)
-	plt.pause(0.0001)
-		
-plt.close()
+    data = ser.readline()
+    print(data.decode())
+    r = float(data.decode())
+    angle = angle + 5
+    if angle >= 360.0:
+        angle = angle - 360
+        plt.clf()
+        plt.axis([-9, 9, -9, 9])
+        plt.scatter(0, 0, c="black", marker='s', s=5.5)
+    sine = math.sin(math.radians(angle))
+    cosine = math.cos(math.radians(angle))
+    plt.scatter(r * cosine, r * sine, s=0.4, c="red")
+    plt.pause(0.001)
