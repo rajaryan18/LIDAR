f7dd533da8619428f888aa7d8161959fe1bafca2
ayushsh314 <90515247+ayushsh314@users.noreply.github.com>
2021-10-03 20:40:35 +0530

    Add files via upload
    new updated plot code

--- plotGraph.py
+++ plotGraph.py
@@ -1,30 +1,42 @@
 import serial
 import matplotlib.pyplot as plt
 import math
+import time
 
 plt.ion()
 fig = plt.figure()
 plt.axis([-9, 9, -9, 9])
-plt.scatter(0, 0, c="black", marker='s', s=5.5)
-ser = serial.Serial('COM4', 9600) #change to higher baud rate(>9600)
+plt.scatter (0, 0, c="black", marker='s', s=5.5)
+ser = serial.Serial('COM3', 115200)
+
 angle = 0
-x = 0
+x=0
 while True:
-    data = ser.readline()
-    print(data.decode())
-    r = float(data.decode())
+    data = ser.readline()#reading from serial monitor
+    try:
+        print(data.decode())
+    except:pass
+    r = 0
+    try :
+        r = float(data.decode())
+    except : pass
+    begin=time.time()
+    if r > 0.10:
+        r = 0.10
     angle = angle + 3
-    if angle >= 360.0:
+    if angle >= 360.0:     #resetting everything
         angle = angle - 360
         plt.clf()
         plt.axis([-9, 9, -9, 9])
-        plt.scatter(0, 0, c="black", marker='s', s=5.5)
+    
     sine = math.sin(math.radians(angle))
     cosine = math.cos(math.radians(angle))
     plt.scatter(r * cosine * 100, r * sine * 100, s=4.4, c="red")
     plt.pause(0.001)
-    x = x + 1
-    if x == 216:#after two rotations
+    #end=time.time()
+    print(end-begin)
+    x=x+1
+    if x==360:
         break
-        
+
 plt.close()
